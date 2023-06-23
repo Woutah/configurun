a314a8f065da59188e9937cb33cc564214b7d351
Wouter <wouterstokman@hotmail.com>
2023-06-23 11:53:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-	name = "MLQueue",
-	version= "1.0.0",
-	packages=find_packages('.')
+	name = "configurun",
+	version= "0.1.0",
+	packages=find_packages('.'),
+    description="PySide6 based user-interface tools to create and run experiment-configurations with Python.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Wouter Stokman",
+    url="https://github.com/Woutah/configurun",
+    license="LGPLv2",
+    include_package_data=True,
+    install_requires=[
+		"PySide6",
+	]
 )
